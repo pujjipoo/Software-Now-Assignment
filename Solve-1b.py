84d8eb4cefa276e72190c6f1a6a96c60e2b8fd06
Mohammed Ashrafujjaman <mashraf9881@gmail.com>
2025-08-02 12:52:03 +1000

    initializing file structure

---
+++ Solve-1b.py
@@ -0,0 +1,22 @@
+'''
+Group Name: Sydney Group 17
+Group Members: 4
+Mohammed Ashrafujjaman Hera - 391197
+Pujan Dey  - 395076
+Shaown Imtiaz - 396121
+Al-Amin Dhaly - 395230
+
+'''
+# square box print using stars
+
+# input taking -> pujan
+
+#input validation
+    # integer -> pujan
+    # float -> al-amin
+    # char/str -> ashraf
+    
+#logical implementation
+    # loop -> Imtiaz
+    
+#result -> al-amin
--- Solve_1a.py
+++ Solve_1a.py
@@ -1,1 +1,29 @@
+'''
+Group Name: Sydney Group 17
+Group Members: 4
+Mohammed Ashrafujjaman Hera - 391197
+Pujan Dey  - 395076
+Shaown Imtiaz - 396121
+Al-Amin Dhaly - 395230
 
+'''
+#triangle 
+
+# basic input -> Imtiaz
+
+# input validation
+
+    # basic integer -> Imtiaz
+    
+    # float -> Pujon
+    
+    # char/str -> al-amin
+    
+# logical implementation
+
+    # a,b,c>0 -> al-amin
+    # a+b>c logic -> ashraf
+    # b+c>a logic -> pujan
+    # c+a>b logic -> imtiaz
+
+# result -> ashraf
