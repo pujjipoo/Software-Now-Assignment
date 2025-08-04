738cdc9d6066c15225dd7a5a7c6ec5f54d0e2736
Mohammed Ashrafujjaman <mashraf9881@gmail.com>
2025-08-04 13:21:20 +1000

    solve_1A done

--- Solve_1a.py
+++ Solve_1a.py
@@ -7,52 +7,55 @@ Shaown Imtiaz - 396121
 Al-Amin Dhaly - 395230
 '''
 
-# triangle
+def input_validation(x):
+    # integer -> Ashraf
+    try:
+        # Trying to convert string input from user into a Interger number
+        temp_x = int(x)
+        x = temp_x
+    except ValueError:
+        try:
+            # float -> Pujan
+            pass
+        except ValueError:
+            # char/str -> al-amin
+            pass
+    return x
+            
 
-# basic input -> Imtiaz
-a_input = input("User input 1: ")
-b_input = input("User input 2: ")
-c_input = input("User input 3: ")
+# code for inputing three numbers, and check if these three numbers can form a triangle.
+restart = True
+while restart:
+    # basic input -> Imtiaz
+    # Taking 3 side input from user
+    a_input = input("User input 1: ")
+    b_input = input("User input 2: ")
+    c_input = input("User input 3: ")
 
-# input validation
+    # input validation
+    a = input_validation(a_input)
+    b = input_validation(b_input)
+    c = input_validation(c_input)
 
-try:
-    # Try converting inputs to float
-    a = float(a_input)
-    b = float(b_input)
-    c = float(c_input)
-
-    print(f"Inputs converted to float successfully: {a}, {b}, {c}")
-
-    # After this, pass to next team member for logic (a+b>c etc.)
+    # manual debuging
+    # print(f"a: {a}\nb: {b}\nc: {c}")
     
-except ValueError:
-    # If user entered text or invalid number, this block will run
-    print("Error: Please enter valid numbers only for triangle sides.")
+    if a <= 0 or b <= 0 or c <= 0:
+        print("NO, these three lengths CANNOT form a triangle.")
+    # a + b > c -> condition cheking if a + b greater than c (Ashraf)
+    elif a + b > c :
+        # b + c > a -> (Imtiaz) condition cheking if b + c greater than a
+        # al-amin will implement his code here
+        pass # remove this al-amin
+       
+    else: 
+        print("NO, these three lengths CANNOT form a triangle.")
 
+    # asking user if they want to restart the program again.
+    # Al-amin
+    choice = input("Press n/no to exit or Press any other button to restart: ")
+    if choice == "n" or choice == "no":
+        restart = False
     
-    # char/str -> al-amin
-if a <= 0 or b <= 0 or c <= 0:
-    print("Invalid input: Side lengths must be positive numbers.")
-else:
-    # a + b > c -> Ashraf
-    cond1 = a + b > c
-    # b + c > a -> Pujan
-    cond2 = b + c > a
-    # c + a > b -> Imtiaz
-    cond3 = c + a > b
-
-    # Result display -> Ashraf
-    if cond1 and cond2 and cond3:
-        print("Yes, these three lengths can form a triangle.")
-    else:
-        print("NO, these three lengths CANNOT form a triangle.")   
-
-# logical implementation
-
-    # a,b,c>0 -> al-amin
-    # a+b>c logic -> ashraf
-    # b+c>a logic -> pujan
-    # c+a>b logic -> imtiaz
-
-# result -> ashraf
+        
+        
