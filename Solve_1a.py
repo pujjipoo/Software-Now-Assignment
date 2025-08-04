e6b5213b28de1a2c8cc285a3e4466c17f6c7b9d5
Al-Amin <alamindhaly114@gmail.com>
2025-08-04 21:24:46 +1000

    Updated Solve_1a.py with latest changes

--- Solve_1a.py
+++ Solve_1a.py
@@ -10,7 +10,7 @@ Al-Amin Dhaly - 395230
 def input_validation(x):
     # integer -> Ashraf
     try:
-        # Trying to convert string input from user into an Integer number
+        # Trying to convert string input from user into a Interger number
         temp_x = int(x)
         x = temp_x
     except ValueError:
@@ -18,7 +18,6 @@ def input_validation(x):
             # float -> Pujan
             pass
         except ValueError:
-            # char/str -> al-amin
             # In case of character input below code will convert it to integer based on Ascii value
             # Below code only take 1st char of a string to convert into integer
             if x == "":
@@ -28,6 +27,7 @@ def input_validation(x):
                 temp_x = ord(x[0])
                 x = temp_x
                 print(f"user provided a char or string. Converting first char into Ascii equivalent: {x}")
+            
     return x
             
 
@@ -45,27 +45,30 @@ while restart:
     b = input_validation(b_input)
     c = input_validation(c_input)
 
-    # manual debugging
+    # manual debuging
     # print(f"a: {a}\nb: {b}\nc: {c}")
     
     if a <= 0 or b <= 0 or c <= 0:
         print("NO, these three lengths CANNOT form a triangle.")
-    # a + b > c -> condition checking if a + b greater than c (Ashraf)
-    elif a + b > c:
-        # b + c > a -> (Imtiaz) condition checking if b + c greater than a
-        if b + c > a:
-            # Al-amin implementing the last condition: c + a > b
-            if c + a > b:
-                print("YES, these three lengths CAN form a triangle.")
-            else:
-                print("NO, these three lengths CANNOT form a triangle.")
+    # a + b > c -> condition cheking if a + b greater than c (Ashraf)
+
+    elif a + b > c :
+
+        # b + c > a -> (Imtiaz) condition cheking if b + c greater than a
+        if c + a > b:
+            # c + a > b -> (Alamin) condition cheking if c + a greater than b
+            print("YES, these three lengths CAN form a triangle.")
         else:
             print("NO, these three lengths CANNOT form a triangle.")
-    else: 
+    else:
         print("NO, these three lengths CANNOT form a triangle.")
+        
 
     # asking user if they want to restart the program again.
     # Al-amin
     choice = input("Press n/no to exit or Press any other button to restart: ")
-    if choice.lower() == "n" or choice.lower() == "no":
+    if choice == "n" or choice == "no":
         restart = False
+    
+        
+        
