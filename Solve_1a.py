01261a5eca13475f1558f4a0cd03cb06605211b3
Mohammed Ashrafujjaman <mashraf9881@gmail.com>
2025-08-09 17:12:49 +1000

    final formating done

--- Solve_1a.py
+++ Solve_1a.py
@@ -7,50 +7,67 @@ Shaown Imtiaz - 396121
 Al-Amin Dhaly - 395230
 '''
 
+# code for taking input of three side of a triangle, and check if these three sides length can form a triangle.
+# -------------------------------------------------------------------------------------------------------------
+
 # This function validate input received form user. Also this code is inspired from class lecture.
-def input_validation(x):
+
+def input_validation(x, side_num):
     try:
         # integer -> Ashraf
         # Trying to convert string input from user into a Interger number
         temp_x = int(x)
         x = temp_x
+        print(f"\nSide: {side_num}")
+        print("User provided an integer number.")
     except ValueError:
         try:
             # float -> Pujan
-            pass
+            float_x = float(x)
+            round_x = round(float_x)
+            x = int(round_x)
+            print(f"\nSide: {side_num}")
+            print(f"User provided a float value for the side {side_num}.\nConverting it to integer.\n")
         except ValueError:
             # char/str -> al-amin
             # In case of character input below code will convert it to integer based on Ascii value
             # Below code only take 1st char of a string to convert into integer
             if x == "":
                 x = 7 # placing a default value 7
-                print("user did not provide any value. Default value 7")
+                print(f"\nSide: {side_num}")
+                print(f"user did not provide any value for side {side_num}.\nDefault value Assigned.\n")
             else:
                 temp_x = ord(x[0])
                 x = temp_x
-                print(f"user provided a char or string. Converting first char into Ascii equivalent: {x}")
+                print(f"\nSide: {side_num}")
+                print(f"user provided a character or string for side {side_num}.\nConverting only first character into Ascii equivalent number.\n")
     return x
             
 
-# code for inputing three numbers, and check if these three numbers can form a triangle.
-# This code can run repeatedly. 
+
+# This code can run repeatedly, until user says "n" or "no" (lower case) when program will ask for another try.
 restart = True
 while restart:
     # basic input -> Imtiaz
-    # Taking 3 side input from user
-    a_input = input("User input 1: ")
-    b_input = input("User input 2: ")
-    c_input = input("User input 3: ")
+    # Taking 3 sides length from user
+    a_input = input("Length of side 1: ")
+    b_input = input("Length of side 2: ")
+    c_input = input("Length of side 3: ")
 
     # input validation
-    a = input_validation(a_input)
-    b = input_validation(b_input)
-    c = input_validation(c_input)
+    # Here a,b,c are the three sides of a triangle
+    a = input_validation(a_input,1)
+    b = input_validation(b_input,2)
+    c = input_validation(c_input,3)
 
     # manual debuging
     # print(f"a: {a}\nb: {b}\nc: {c}")
     
-if a <= 0 or b <= 0 or c <= 0:
+    print(f"\nFinal lengths:\nLength of side 1: {a}\nLength of side 2: {b}\nLength of side 3: {c}\n")
+    
+    print("Final Outcome: ")
+    # checking condition for side length,if any one of them is zero or not
+    if a <= 0 or b <= 0 or c <= 0:
         print("NO, these three lengths CANNOT form a triangle.")
     # a + b > c -> condition cheking if a + b greater than c (Ashraf)
     elif a + b > c :
@@ -66,11 +83,11 @@ if a <= 0 or b <= 0 or c <= 0:
     else:
         print("NO, these three lengths CANNOT form a triangle.")
 
-    # asking user if they want to restart the program again.
-    # Al-amin
-    choice = input("Press n/no to exit or Press any other button to restart: ")
+    # asking user if they want to restart the program again. --> (Al-Amin)
+    choice = input("\nPress n/no to exit or Press any other button to restart: ")
     if choice == "n" or choice == "no":
         restart = False
-    
+        
+# End of code
         
 
--- Solve_1b.py
+++ Solve_1b.py
@@ -9,35 +9,33 @@ Al-Amin Dhaly - 395230
 '''
 # taking input from user
 size_input = input("Enter the size of the square: ")
-sq_size = 0
+
 # Input validation. This code is inspired by class lecture
 try:
     # Input validation
     # Integer -> Pujan 
-    pass
-
-except ValueError:
-    # print(f"An unexpected error occurred: {e}")
-    
+    sq_size = int(size_input)
+    print("\nUser provided an integer number.")
+except ValueError:   
     try:
         # Check if float -> Al-Amin
-        pass
-        
+        sq_size = int(round(float(size_input)))
+        print("\nUser provided a float value.\nConverting it to integer.\n")
     except ValueError:
         # print("Invalid input: Please enter a numeric value.")
         # Check if char/str -> Ashraf
         if size_input == "":
             sq_size = 5 # default value in case user does not provide any value
+            print("\nuser did not provide any value.\nDefault value Assigned.\n")
         else: 
             sq_size = ord(size_input[0])
+            print(f"\nuser provided a character or string.\nConverting only first character into Ascii equivalent number.\n")
 # ---------------------------------------
 # Logical implementation
 # Loop -> Imtiaz
+print(f"printing the square below (size {sq_size}): \n")
 for i in range(sq_size):
     if i == 0 or i == sq_size - 1:
         print("* " * sq_size)
     else:
-        if sq_size == 1 or sq_size == 2:
-            print("* " * sq_size)
-        else:
-            print("*" + "  " * (sq_size - 2) + " *")
+        print("*" + "  " * (sq_size - 2) + " *")
